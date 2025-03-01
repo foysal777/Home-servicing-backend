2118b53ea025a8726d2e012fa9b6ab6ee89aca01
foysal777 <foysal.cse11@gmail.com>
2025-03-01 12:13:07 +0600

    Add Category Api

--- HomeService/settings.py
+++ HomeService/settings.py
@@ -38,11 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',  
     # my apps
     'categories',
 ]
-
+ 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True  
 ROOT_URLCONF = 'HomeService.urls'
 
 TEMPLATES = [
--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    icon = models.CharField(max_length=500)  
+    listings = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
